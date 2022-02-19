8654d012ea811b261a6d050aa769d2d8cedea587
prasadweb86 <prasad.web86@gmail.com>
2022-02-19 13:51:10 +0530

    mobile responsive

--- src/app/views/common/mainmenu/mainmenu.component.ts
+++ src/app/views/common/mainmenu/mainmenu.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-mainmenu',
   templateUrl: './mainmenu.component.html',
-  styleUrls: ['./mainmenu.component.scss']
+  styleUrls: ['./mainmenu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainmenuComponent implements OnInit {
 
