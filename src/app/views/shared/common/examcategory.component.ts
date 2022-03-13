51946c35115023a28b598e681791d94d2f17131b
prasadweb86 <prasad.web86@gmail.com>
2022-03-13 09:29:46 +0530

    categoryes

---
+++ src/app/views/shared/common/examcategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examcategory',
+  templateUrl: './examcategory.component.html',
+  styleUrls: ['./examcategory.component.scss']
+})
+export class ExamcategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
